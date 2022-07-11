599c283d1deddaebcf78cc0daf234fdbba628233
priya <priyapandey8884.blr@gmail.com>
2022-07-11 18:06:47 +0530

    Added:Taking more than three parameters

--- src/maximum/Main.java
+++ src/maximum/Main.java
@@ -3,10 +3,9 @@ package maximum;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Integer of max are:"+Maximum.toPrintMax(2,3,6));
-		System.out.println("Float of maximum values are:" +Maximum.toPrintMax(6.6f,8.8f,10.7f));
-		System.out.println("String of three max are:" +Maximum.toPrintMax("Apple","Banana","Oranges"));
-
+		Maximum.<Integer>toPrintMax(3,4,5,6);
+		Maximum.<String>toPrintMax("Apple","Pears","Banana");
+		Maximum.<Float>toPrintMax(2.2f,4.5f,9.8f,10.6f,6.7f);
 	}
 
 }
