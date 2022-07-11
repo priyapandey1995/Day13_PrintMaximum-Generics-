f664640dcd9537f30a07de8c9fbfd959b2548a17
priya <priyapandey8884.blr@gmail.com>
2022-07-11 16:32:02 +0530

    Added:To Find maximum of Three Strings

--- src/maximum/MaxStrings.java
+++ src/maximum/MaxStrings.java
@@ -1,34 +1,32 @@
 package maximum;
 
-public class MaxFloats {
-	
-	/*
-	 * taken 3 floating variables and its compared with each other
+public class MaxStrings {
+
+	/* taken 3 floating variables and its compared with each other
 	 * to check for maximum, float object is used and also compareTo method 
 	 * is applied 
 	 */
-	public static Float toPrintMax(Float a,Float b,Float c) {
-		
+	public static String toPrintMax(String a,String b,String c) {
+
 		//initially taken a as max
-		Float max = a;
-		
+		String max = a;
+
 		/*
 		 * first,b is compared with max,if its true,it returns as max
 		 * if not,it checks for another condition
-		*/
-		
+		 */
 		if(b.compareTo(max) > 0)
 			max = b;
-		
+
 		//c checks for max
 		if(c.compareTo(max) > 0)
 			max = c;
-		
-		System.out.println("First Number=" +a);
-		System.out.println("Second Number=" +b);
-		System.out.println("Third Number=" +c);
+
+		System.out.println("First String="+ " " +a);
+		System.out.println("Second Second=" +" " +b);
+		System.out.println("Third String=" + " " +c);
 		//Maximum is printed
-		System.out.println("Maximum Value is="+ max);
+		System.out.println("Maximum  is="+  " " +max);
 		return max;
 	}
 }
