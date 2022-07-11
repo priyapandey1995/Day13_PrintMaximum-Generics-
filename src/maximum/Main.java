fe7a5a52a19740cd52ac635345eb2880af1bb974
priya <priyapandey8884.blr@gmail.com>
2022-07-11 17:18:20 +0530

    Added:Maximum of three number using generics

--- src/maximum/Main.java
+++ src/maximum/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Integer of max are:"+Maximum.toPrintMax(2,3,6));
-		System.out.println("Float of maximum values are:" +Maximum.toPrintMax(2.2f,3f,10f));
+		System.out.println("Float of maximum values are:" +Maximum.toPrintMax(6.6f,8.8f,10.7f));
 		System.out.println("String of three max are:" +Maximum.toPrintMax("Apple","Banana","Oranges"));
 
 	}
