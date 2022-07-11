a62116049bf5babdd2cc04d569da1b9360c49303
priya <priyapandey8884.blr@gmail.com>
2022-07-11 16:52:00 +0530

    Added:RefactorOne-Maximum of Three numbers

--- src/maximum/Main.java
+++ src/maximum/Main.java
@@ -3,7 +3,9 @@ package maximum;
 public class Main {
 
 	public static void main(String[] args) {
-		MaxStrings.toPrintMax("Apple","Peach","Pomogranate");
+		System.out.println("Integer of max are:"+Maximum.toPrintMax(2,3,6));
+		System.out.println("Float of maximum values are:" +Maximum.toPrintMax(2.2f,3f,10f));
+		System.out.println("String of three max are:" +Maximum.toPrintMax("Apple","Banana","Oranges"));
 
 	}
 
